3318f695ff5ce1f84c38667f36bdb2a22d20903b
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-01-16 21:29:54 +0100

    fix: remove extra curly brace

--- USBMidiAndroid/USBMidiAndroid/src/main/java/com/arthaiirgames/usbmidiandroid/UsbMidiController.java
+++ USBMidiAndroid/USBMidiAndroid/src/main/java/com/arthaiirgames/usbmidiandroid/UsbMidiController.java
@@ -36,7 +36,6 @@ public class UsbMidiController {
 
     public void ctor(IMidiCallback callback, @NonNull Activity activity)
     {
-    {
         _context = activity.getApplicationContext();
         _midiCallback = callback;
         registerMidiDevices();
