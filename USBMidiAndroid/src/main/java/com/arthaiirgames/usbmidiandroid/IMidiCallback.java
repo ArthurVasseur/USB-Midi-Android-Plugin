73eee755799fbfc6c1cbe1b8b9af272769fd7c60
Bart Trzynadlowski <bart.trzy@gmail.com>
2023-02-11 17:23:51 -0800

    Fixes to threading, callbacks, receivers, etc.
    - Only a single BroadcastReceiver for USB events is now created.
      Previously, a new one was being created each time a USB device was
      attached (even if it was the same device being re-attached), causing
      an additional redundant onReceive() call each time a MIDI device was
      disconnected and then reconnected.
    - On device detachment, USB reader threads are stopped (although we do
      not actually join() them; seems to work ok for now).
    - The DeviceAttached callback will now fire if the device is already
      plugged in.
    - Added a RawMidi callback that fires on any MIDI data received and
      always before NoteOn/NoteOff.

--- USBMidiAndroid/src/main/java/com/arthaiirgames/usbmidiandroid/IMidiCallback.java
+++ USBMidiAndroid/src/main/java/com/arthaiirgames/usbmidiandroid/IMidiCallback.java
@@ -1,8 +1,9 @@
 package com.arthaiirgames.usbmidiandroid;
 
 public interface IMidiCallback {
-    void NoteOn(int note, int velocity);
-    void NoteOff(int note);
     void DeviceAttached(String name);
     void DeviceDetached(String name);
+    void RawMidi(byte command, byte data1, byte data2);
+    void NoteOn(int note, int velocity);
+    void NoteOff(int note);
 }
